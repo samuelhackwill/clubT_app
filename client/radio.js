751b162473b65cb7bbecaade27e7d6811b5228ef
Samuel <hackmew@gmail.com>
2021-06-12 11:33:11 +0200

    Add stuff

--- client/radio.js
+++ client/radio.js
@@ -12,10 +12,28 @@ Template.radioBrowser.onRendered(function(){
 
 Template.radioMobile.onRendered(function(){
  document.body.style.opacity=1
+
+ Session.set("playing", false)
 })
 
 Template.radioMobile.helpers({
 	live(){
 		return true
+	},
+
+	activeLink(arg){
+		console.log(arg.hash.arg)
+	},
+
+	playing(){
+		return Session.get("playing")
 	}
+})
+
+Template.radioMobile.events({
+
+	'click .Direct'(){
+		Session.set("playing", true)
+	}
+
 })
