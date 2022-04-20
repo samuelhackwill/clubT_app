fc5690f9ce022d5dfb34463af6fae44b0ae3cb0c
Samuel Hackwill <hackmew@gmail.com>
2022-04-20 17:28:23 +0200

    Add basic fontEnd for tarot rules

--- client/tarot.js
+++ client/tarot.js
@@ -15,6 +15,15 @@ Template.tarot.onRendered(function(){
 	document.body.style.opacity=1
 	$(document.body).addClass('tarot');
 
+	var allAudio = document.getElementsByClassName("soundBytes");
+	
+	for (var i = document.getElementsByClassName("soundBytes")[0].children.length - 1; i >= 0; i--) {
+		document.getElementsByClassName("soundBytes")[0].children[i].children[1].onended=hideSelf
+	}
+	// audio1.onended = function() {
+	// 	alert("audio playback has ended");
+	// };
+
 })
 
 Template.tarot.helpers({
@@ -25,3 +34,9 @@ Template.tarot.helpers({
 Template.tarot.events({
 
 })
+
+hideSelf = function(){
+	console.log(this)
+	this.parentElement.style.backgroundColor="beige"
+	this.parentElement.style.opacity=0.5
+}
